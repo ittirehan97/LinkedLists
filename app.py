371f5b581d7bba7bac9efd4fec7bda101b1a1a39
Itti Rehan <ittirehan97@gmail.com>
2017-12-15 12:30:18 +0530

    Create app.py
    This is the class through which we control the Linked List or implement the Linked List. We create an object of the Linked List and we insert or remove nodes using this.

---
+++ app.py
@@ -0,0 +1,15 @@
+from LinkedListDS.LinkedList import LinkedList
+
+l1 = LinkedList()
+
+l1.insertStart(12)
+l1.insertEnd(156)
+l1.insertEnd(213)
+l1.insertEnd(212)
+
+l1.traverseList()
+
+l1.remove(12)
+l1.insertStart(100)
+
+l1.traverseList()
